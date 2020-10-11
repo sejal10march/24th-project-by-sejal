e573a5b272aac69cae19df9f2a87c87c0c58b312
sejal10march <70520738+sejal10march@users.noreply.github.com>
2020-10-11 20:06:30 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -3,7 +3,8 @@ class Dustbin {
       var options = {
           'restitution':0.8,
           'friction':1.0,
-          'density':1.0
+          'density':1.0,
+          'isStatic':true
       }
       this.body = Bodies.rectangle(x, y,width, height, options);
       this.width = width;
